f32fd5676ab4768340dedcb7ab2c4c10a31a2d7b
Raapchik <priyankdegami1@gmail.com>
2018-09-10 07:14:51 +0530

    Two Islands Added

--- IGDC/Assets/ColorWheel.cs
+++ IGDC/Assets/ColorWheel.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class ColorWheel : MonoBehaviour {
 
 	public GameObject obj;
+	private Vector3 rot=new Vector3(0,90f,0);
+	public bool counter = true;
+	public int pass;
+
+	void Update()
+	{
+		transform.rotation = Quaternion.identity;
+	}
 
 	void OnTriggerStay(Collider col)
 	{
@@ -12,12 +20,41 @@ public class ColorWheel : MonoBehaviour {
 		if (col.tag == "Player")
 			Debug.Log (col.gameObject.name);
 
-		if (Input.GetKeyDown (KeyCode.E)) 
+		if (Input.GetKeyDown (KeyCode.E) && counter) 
 		{
 			Debug.Log ("E Pressed.");
-			//obj.transform.rotation = Quaternion.Lerp (transform.rotation,Quaternion.Lerp(90,0,0), 0.5);
+			//obj.transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler(new Vector3(90f,transform.rotation.y,transform.rotation.z)), Time.time*0.1f);
+			//StartCoroutine(Rot());
+			counter=false;
+			StartCoroutine (Rotate (Vector3.up, 90, 1));
 
 
 		}
+	}
+
+	IEnumerator Rot()
+	{
+		obj.transform.localRotation= Quaternion.Lerp (transform.rotation, Quaternion.Euler(new Vector3(90f,transform.rotation.y,transform.rotation.z)), Time.time*0.1f);
+		yield return new WaitForSeconds (1f);
+	}
+
+	IEnumerator Rotate( Vector3 axis, float angle, float duration = 1.0f)
+	{
+		Quaternion from = obj.transform.rotation;
+		Quaternion to = obj.transform.rotation;
+		to *= Quaternion.Euler( axis * angle );
+
+		float elapsed = 0.0f;
+		while( elapsed < duration )
+		{
+			obj.transform.rotation = Quaternion.Slerp(from, to, elapsed / duration );
+			elapsed += Time.deltaTime;
+			yield return null;
+			if (elapsed / duration >=1f) 
+			{
+				counter = true;
+			}
+		}
+		transform.rotation = to;
 	}
 }
---
+++ IGDC/Assets/PillarRot.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarRot : MonoBehaviour {
+
+	public List<GameObject> gb;
+	private bool counter=true;
+
+	void OnTriggerStay(Collider col)
+	{
+		Debug.Log ("Aya");
+		if (col.tag == "Player")
+			Debug.Log (col.gameObject.name);
+
+		if (Input.GetKeyDown (KeyCode.E) && counter) 
+		{
+			Debug.Log ("E Pressed.");
+			//obj.transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler(new Vector3(90f,transform.rotation.y,transform.rotation.z)), Time.time*0.1f);
+			//StartCoroutine(Rot());
+			counter=false;
+			StartCoroutine (Rotate (Vector3.up, 90, 1));
+
+
+		}
+	}
+
+	IEnumerator Rotate( Vector3 axis, float angle, float duration = 1.0f)
+	{
+		foreach (GameObject i in gb) {
+			Quaternion from = i.transform.rotation;
+			Quaternion to = i.transform.rotation;
+			to *= Quaternion.Euler (axis * angle);
+
+
+			float elapsed = 0.0f;
+			while (elapsed < duration) {
+				i.transform.rotation = Quaternion.Slerp (from, to, elapsed / duration);
+				elapsed += Time.deltaTime;
+				yield return null;
+				if (elapsed / duration >= 1f) {
+					counter = true;
+				}
+			}
+			transform.rotation = to;
+		}
+	}
+}
